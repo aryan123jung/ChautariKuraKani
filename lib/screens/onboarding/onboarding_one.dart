f494f773c4054074c47ca183d7f3cee729478e3e
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-12-04 16:03:43 +0545

    updated onboarding one

--- lib/screens/onboarding/onboarding_one.dart
+++ lib/screens/onboarding/onboarding_one.dart
@@ -1,5 +1,6 @@
+import 'package:chautari_kurakani/screens/onboarding/onboarding_two.dart';
 import 'package:chautari_kurakani/widgets/my_floating_button.dart';
-import 'package:chautari_kurakani/widgets/my_outline_button.dart';
+// import 'package:chautari_kurakani/widgets/my_outline_button.dart';
 import 'package:flutter/material.dart';
 
 class OnboardingOne extends StatefulWidget {
@@ -46,7 +47,9 @@ class _OnboardingOneState extends State<OnboardingOne> {
               padding: const EdgeInsets.all(20.0),
               child: MyFloatingButton(
                 
-                onPressed: () {}, 
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>OnboardingTwo()));
+                }, 
                 text: "Next",
                 color: const Color.fromARGB(255, 229, 163, 32),
               ),

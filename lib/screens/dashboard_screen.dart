bdf30e3781f44a543d0d1f2f33402275938b481d
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-12-16 20:56:26 +0545

    updated bottom nav

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -35,8 +35,7 @@ class _BottomNavScreenState extends State<DashboardScreen> {
           BottomNavigationBarItem(icon: Icon(Icons.message),label: 'Message'),
           BottomNavigationBarItem(icon: Icon(Icons.person),label: 'Profile'),
         ],
-        selectedItemColor: Colors.black,
-        unselectedItemColor: const Color.fromARGB(255, 224, 224, 224),
+        
         currentIndex: _selectedIndex,
         onTap: (index) {
           setState(() {
--- lib/theme/theme_data.dart
+++ lib/theme/theme_data.dart
@@ -4,7 +4,13 @@ ThemeData getApplicationTheme(){
   return ThemeData(
     // fontFamily: 'OpenSans Regular',
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      backgroundColor: const Color(0XFF76C05D)
-    )
+      backgroundColor: const Color(0XFF76C05D),
+      selectedItemColor: Colors.black,
+      unselectedItemColor: const Color.fromARGB(255, 224, 224, 224),
+      selectedLabelStyle: TextStyle(fontFamily: "OpenSans Bold"),
+      unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+      selectedIconTheme: IconThemeData(size: 30)
+    ),
+
   );
 }

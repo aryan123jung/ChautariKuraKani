4659f834f3b9e970dc0b1ccaf22ad4d5d0f163d6
aryan123jung <149761592+aryan123jung@users.noreply.github.com>
2025-12-19 17:22:21 +0545

    final push

--- lib/screens/bottom_nav_screen/home_screen/chautari_screen.dart
+++ lib/screens/bottom_nav_screen/home_screen/chautari_screen.dart
@@ -1,32 +1,3 @@
-// import 'package:chautari_kurakani/models/chautari-model.dart';
-// import 'package:chautari_kurakani/widgets/chautari_card_widget.dart';
-// import 'package:flutter/material.dart';
-
-// class ChautariScreen extends StatelessWidget {
-//   const ChautariScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final List<ChautariModel> posts = [
-//       ChautariModel(
-//         name: "c/Kurakani",
-//         caption: "Join and feel free to express yourself ;)",
-//         imageUrl:
-//             "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Cute_dog.jpg/2560px-Cute_dog.jpg",
-//       ),
-//     ];
-//     return ListView.builder(
-//       itemCount: posts.length,
-//       itemBuilder: (context,index){
-//         return ChautariCardWidget(post: posts[index]);
-//       },
-      
-//       );
-      
-//   }
-// }
-
-
 import 'package:chautari_kurakani/models/chautari_model.dart';
 import 'package:chautari_kurakani/widgets/chautari_card_widget.dart';
 import 'package:flutter/material.dart';
@@ -73,7 +44,7 @@ class ChautariScreen extends StatelessWidget {
       body: Stack(
         children: [
           ListView.builder(
-            padding: const EdgeInsets.only(bottom: 80), // space for FAB
+            padding: const EdgeInsets.only(bottom: 80), 
             itemCount: posts.length,
             itemBuilder: (context, index) {
               return ChautariCardWidget(post: posts[index]);

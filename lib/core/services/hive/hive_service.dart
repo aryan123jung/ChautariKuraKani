0dad3844e8ce5b316986a686683e5eacd6dff351
Aryan Jung Chhetri <aryanjungchhetri@Aryans-MacBook-Pro.local>
2026-01-01 23:31:51 +0545

    datalayer completed

--- lib/core/services/hive/hive_service.dart
+++ lib/core/services/hive/hive_service.dart
@@ -1,7 +1,12 @@
 import 'package:chautari_kurakani/core/constants/hive_table_constant.dart';
 import 'package:chautari_kurakani/features/auth/data/models/auth_hive_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+
+final hiveServiceProvider = Provider<HiveService>((ref) {
+  return HiveService();
+});
 
 class HiveService {
   //Initialize hive
@@ -18,80 +23,79 @@ class HiveService {
     if (!Hive.isAdapterRegistered(HiveTableConstant.authTypeId)) {
       Hive.registerAdapter(AuthHiveModelAdapter());
     }
-
-    // if (!Hive.isAdapterRegistered(HiveTableConstant.categoryTypeId)) {
-    //   Hive.registerAdapter(AuthHiveModelAdapter());
   }
-}
 
-//Open all boxes
-Future<void> _openBoxes() async {
-  await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
-
-  // await Hive.openBox<CategoryHiveModel>(HiveTableConstant.categoryTable);
-}
-
-// Delete all auths
-Future<void> deleteAllAuths() async {
-  await _authBox.clear();
-
-  // await _categoryBox.clear();
-}
-
-// Close all boxes
-Future<void> close() async {
-  await Hive.close();
-}
-
-// ==================== Auth Queries ====================
-
-// Get auth box
-Box<AuthHiveModel> get _authBox =>
-    Hive.box<AuthHiveModel>(HiveTableConstant.authTable);
-
-// Register
-Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
-  await _authBox.put(model.authId, model);
-  return model;
-}
-
-//Login
-Future<AuthHiveModel?> loginUser(String email, String password) async {
-  final users = _authBox.values.where(
-    (user) => user.email == email && user.password == password,
-  );
-  if (users.isNotEmpty) {
-    return users.first;
+  //Open all boxes
+  Future<void> _openBoxes() async {
+    await Hive.openBox<AuthHiveModel>(HiveTableConstant.authTable);
   }
-  return null;
+
+  // Delete all auths
+  Future<void> deleteAllAuths() async {
+    await _authBox.clear();
+  }
+
+  // Close all boxes
+  Future<void> close() async {
+    await Hive.close();
+  }
+
+  // ==================== Auth Queries ====================
+
+  // Get auth box
+  Box<AuthHiveModel> get _authBox =>
+      Hive.box<AuthHiveModel>(HiveTableConstant.authTable);
+
+  // Register
+  Future<AuthHiveModel> registerUser(AuthHiveModel model) async {
+    await _authBox.put(model.authId, model);
+    return model;
+  }
+
+  //Login
+  Future<AuthHiveModel?> loginUser(String email, String password) async {
+    final users = _authBox.values.where(
+      (user) => user.email == email && user.password == password,
+    );
+    if (users.isNotEmpty) {
+      return users.first;
+    }
+    return null;
+  }
+
+  //Logout
+  Future<void> logoutUser() async {
+    // await _authBox.delete(authId);
+  }
+
+  // Get all auth
+  List<AuthHiveModel> getAllAuths() {
+    return _authBox.values.toList();
+  }
+
+  //Get current user
+  AuthHiveModel? getCurrentUser(String authId) {
+    return _authBox.get(authId);
+  }
+
+  // Is email exists
+  bool isEmailExists(String email) {
+    final users = _authBox.values.where((user) => user.email == email);
+    return users.isNotEmpty;
+  }
+
+  // // Get auth by ID
+  // AuthHiveModel? getAuthById(String authId) {
+  //   return _authBox.get(authId);
+  // }
+
+  // // Update a auth
+  // Future<void> updateAuth(AuthHiveModel auth) async {
+  //   await _authBox.put(auth.authId, auth);
+  // }
+
+  // // Delete a auth
+  // Future<void> deleteAuth(String authId) async {
+  //   await _authBox.delete(authId);
+  // }
 }
-
-//Logout
-Future<void> logoutUser(String authId) async {
-  await _authBox.delete(authId);
-}
-
-// Get all auth
-List<AuthHiveModel> getAllAuths() {
-  return _authBox.values.toList();
-}
-
-//Get current user
-AuthHiveModel? getCurrentUser(String authId) {
-  return _authBox.get(authId);
-}
-
-// // Get auth by ID
-// AuthHiveModel? getAuthById(String authId) {
-//   return _authBox.get(authId);
-// }
-
-// // Update a auth
-// Future<void> updateAuth(AuthHiveModel auth) async {
-//   await _authBox.put(auth.authId, auth);
-// }
-
-// // Delete a auth
-// Future<void> deleteAuth(String authId) async {
-//   await _authBox.delete(authId);
-// }
---
+++ lib/features/auth/data/repositories/auth_repository.dart
@@ -0,0 +1,78 @@
+import 'package:chautari_kurakani/core/error/failures.dart';
+import 'package:chautari_kurakani/features/auth/data/datasources/auth_datasource.dart';
+import 'package:chautari_kurakani/features/auth/data/datasources/local/auth_local_datsource.dart';
+import 'package:chautari_kurakani/features/auth/data/models/auth_hive_model.dart';
+import 'package:chautari_kurakani/features/auth/domain/entities/auth_entity.dart';
+import 'package:chautari_kurakani/features/auth/domain/repositories/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Provider
+final authRepositoryProvider = Provider<IAuthRepository>((ref) {
+  return AuthRepository(authDatasource: ref.read(authLocalDatsourceProvider));
+});
+
+class AuthRepository implements IAuthRepository {
+  final IAuthDatasource _authDatasource;
+
+  AuthRepository({required IAuthDatasource authDatasource})
+    : _authDatasource = authDatasource;
+
+  @override
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      final user = await _authDatasource.getCurrentUser();
+      if (user != null) {
+        final entity = user.toEntity();
+        return Right(entity);
+      }
+      return Left(LocalDatabaseFailure(message: 'No user logged in'));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, AuthEntity>> login(
+    String email,
+    String password,
+  ) async {
+    try {
+      final user = await _authDatasource.login(email, password);
+      if (user != null) {
+        final entity = user.toEntity();
+        return Right(entity);
+      }
+      return Left(LocalDatabaseFailure(message: 'Invalid email or password'));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> logout() async {
+    try {
+      final result = await _authDatasource.logout();
+      if (result) {
+        return Right(true);
+      }
+      return Left(LocalDatabaseFailure(message: 'Failed to logout user'));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> register(AuthEntity entity) async {
+    try {
+      final model = AuthHiveModel.fromEntity(entity);
+      final result = await _authDatasource.register(model);
+      if (result) {
+        return Right(true);
+      }
+      return Left(LocalDatabaseFailure(message: 'Failed to register user'));
+    } catch (e) {
+      return Left(LocalDatabaseFailure(message: e.toString()));
+    }
+  }
+}
